f39a51b9854a10702b1447c8642e6cc8b8735dc1
DeliteCode <80368625+DeliteCode@users.noreply.github.com>
2021-08-27 00:23:43 -0300

    version 27 agosto

--- js/site.js
+++ js/site.js
@@ -2,3 +2,55 @@ function helloWorld() {
     alert("Hello World!");
 
 }
+//get the values from interface
+function getValues() {
+    //get values from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //parse into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+    //alert("The start value: " + startValue);
+    
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(startValue, endValue);
+        //call display numbers
+        displayNumbers(numbers);       
+
+    } else {
+        alert("You must enter a valid number");
+    }
+  
+    
+
+}
+
+//generate numbers to start value to end value
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    for (let index = startValue; index <= endValue; index++) {
+       numbers.push(index);
+    }
+
+    return numbers;
+}
+
+//Display the numbers and mark even number in bold
+function displayNumbers(numbers) {
+    let templateRows = ""
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        let className = "even";
+
+        if (number % 2 == 0) {
+            className == "even";
+        } else {
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`; 
+
+    }
+    document.getElementById("results").innerHTML = templateRows;
+}
